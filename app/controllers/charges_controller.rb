4db4b19c20d85954bf65fc730d95fbba39151959
Pierre Lasante <pierre.lasante@videotron.ca>
2020-07-28 10:34:47 -0400

    Refactor home.html.erb

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,24 +4,22 @@ class ChargesController < ApplicationController
 	end
 
 	def create
+	  # Amount in cents
 
-		  # Amount in cents
-		  @amount = 1000
+	  customer = Stripe::Customer.create(
+	    :email => params[:stripeEmail],
+	    :source  => params[:stripeToken]
+	  )
 
-		  customer = Stripe::Customer.create({
-		    email: params[:stripeEmail],
-		    source: params[:stripeToken],
-		  })
+	  charge = Stripe::Charge.create(
+	    :customer    => customer.id,
+	    :amount      => params[:amount],
+	    :description => 'Rails Stripe customer',
+	    :currency    => 'usd'
+	  )
 
-		  charge = Stripe::Charge.create({
-		    customer: customer.id,
-		    amount: @amount,
-		    description: 'Rails Stripe customer',
-		    currency: 'usd',
-		  })
-
-		rescue Stripe::CardError => e
-		  flash[:error] = e.message
-		  redirect_to new_charge_path
-		end
+	rescue Stripe::CardError => e
+	  flash[:error] = e.message
+	  redirect_to new_charge_path
+	end
 end
